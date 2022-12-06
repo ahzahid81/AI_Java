df011171cb841fd93c51d0c6e02e396026458225
ahzahid81 <abdulhasibzahid@gmail.com>
2022-12-06 22:14:34 +0600

    Anisul Islam's Java Video

---
+++ DataType.java
@@ -0,0 +1,29 @@
+
+public class DataType
+{
+    public static void main(String[] args)
+    {
+        boolean b;
+        char c;
+        short s;
+        int i;
+        float f;
+        double d;
+
+        b =true;
+        System.out.println(b);
+        System.out.println("b= " +b);
+
+        c = 'a';
+        s= 5;
+        i = 5;
+        f = 10.2f;
+        d = 10.2;
+        System.out.println("C= "+c);
+        System.out.printf("boolean b =%b\n", b);
+        System.out.printf("Cahracter c = %c\n", c);
+        System.out.printf("Short S = %d\n", s);
+        System.out.printf("float f = %.2f\n", f);
+        System.out.printf("double d = %f\n", d);
+    }
+}
---
+++ InputDemo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class InputDemo 
+{
+    public static void main (String [] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int number;
+
+        System.out.print("Enter any number: ");
+        number = input.nextInt();
+        System.out.println("Number = "+number);
+
+        String name;
+
+        name = "Abdul Hasib Zahid";
+        System.out.println("Welcome "+name);
+    }
+}
